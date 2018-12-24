1c32c0f75d8342280f024bc7abea2c92a712dfe0
Scott Allen <scott@plum.io>
2018-12-24 09:11:44 -0500

    Base selection sort setup

---
+++ selection-sort.py
@@ -0,0 +1,13 @@
+#! /usr/bin/python3
+
+from test_sort import test_sort
+
+
+def selection_sort(array):
+    return array
+
+def main():
+    test_sort(selection_sort)
+
+if __name__ == '__main__':
+    main()
